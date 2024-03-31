23e198fd84f994c2043995fe1f44b28dbc8c7ce0
sh-navid <shamshirsaz.navid@live.com>
2024-03-31 09:13:41 +0330

    Setup step 1 and 2 of simple GAN defined in readme file

--- engine/generative/generative_carpet.py
+++ engine/generative/generative_carpet.py
@@ -1,2 +1,36 @@
-# Generator
-# Discriminator 
+N=5
+
+def creator():
+    """
+        This is the real data creator
+        ___
+
+        Generate (N2+1)x(N2+1) original patterns
+        Like the one below
+        - - - - - + - - - - -
+        - - - - + - + - - - -
+        - - - + - - - + - - -
+        - - + - - - - - + - -
+        - + - - - - - - - + -
+        + - - - - - - - - - +
+        - + - - - - - - - + -
+        - - + - - - - - + - -
+        - - - + - - - + - - -
+        - - - - + - + - - - -
+        - - - - - + - - - - -
+    """
+    pass
+
+# Step 1
+def generator():
+    """
+        This is the fake data generator
+    """
+    pass
+
+# Step 2
+def discriminator():
+    """
+        This is the investigator
+    """
+    pass
