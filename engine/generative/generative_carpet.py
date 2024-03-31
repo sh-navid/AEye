d78ae58ad190164be8ce43ef834dfe02c46d0ab2
sh-navid <shamshirsaz.navid@live.com>
2024-03-31 10:12:18 +0330

    Setup creator

--- engine/generative/generative_carpet.py
+++ engine/generative/generative_carpet.py
@@ -1,36 +1,54 @@
-N=5
+import random
+from n_array import NArray
+
+N = 5
+
 
 def creator():
     """
-        This is the real data creator
-        ___
+    This is the real data creator
+    ___
 
-        Generate (N2+1)x(N2+1) original patterns
-        Like the one below
-        - - - - - + - - - - -
-        - - - - + - + - - - -
-        - - - + - - - + - - -
-        - - + - - - - - + - -
-        - + - - - - - - - + -
-        + - - - - - - - - - +
-        - + - - - - - - - + -
-        - - + - - - - - + - -
-        - - - + - - - + - - -
-        - - - - + - + - - - -
-        - - - - - + - - - - -
+    Generate (N2+1)x(N2+1) original patterns
+    Like the one below
+    - - - - - + - - - - -
+    - - - - + - + - - - -
+    - - - + - - - + - - -
+    - - + - - - - - + - -
+    - + - - - - - - - + -
+    + - - - - - - - - - +
+    - + - - - - - - - + -
+    - - + - - - - - + - -
+    - - - + - - - + - - -
+    - - - - + - + - - - -
+    - - - - - + - - - - -
     """
-    pass
+    D = N + 1
+    sample = NArray.n2(D)
+    x, y = 0, D - 1
+    while x < D:
+        sample[y][x] = 1
+        x += random.choice([0, 1])
+        y -= random.choice([0, 1])
+        if y < 0:
+            y = 0
+    return sample
+
 
 # Step 1
 def generator():
     """
-        This is the fake data generator
+    This is the fake data generator
     """
     pass
+
 
 # Step 2
 def discriminator():
     """
-        This is the investigator
+    This is the investigator
     """
     pass
+
+
+NArray.visualize(creator())
